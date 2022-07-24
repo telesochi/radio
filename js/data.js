4036a9e8def1ba25a92ae3f1559d0428e2826122
Denis Deaf <98868526+telesochi@users.noreply.github.com>
2022-07-24 22:35:13 +0300

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,77 +1,63 @@
 let songs = [
     {
-        name: 'музыка в стиле Italo Disco',
+        name: 'Italo Disco',
         path: 'https://ssl-1.radiohost.pl:8018/stream',
-        artist: 'ITALO4YOU',
+        artist: 'Italo4You',
         cover: 'images/cover/1.jpg'
     },
     
 
 
 {
-        name: 'новости круглые сутки',
-        path: 'https://icecast-vgtrk.cdnvideo.ru/vestifm_aac_32kbps',
-        artist: 'Вести ФМ',
-        cover: 'images/cover/vesti-fm.jpg'
-    },
-
-
-
-
-{
-        name: 'муз. радио в стиле поп',
-        path: 'https://aska.ru-hoster.com:8000/stream',
-        artist: 'Super Radio',
-        cover: 'images/cover/super-radio.jpg'
-    },
-
-
-
-
-{
-        name: 'эпоха Disco 80х и новее',
+        name: '80S Disco',
         path: 'https://pub0102.101.ru:8443/stream/personal/aacp/64/1781512?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJrZXkiOiIwNjBhYzBkMDMzZDExZjg0NTFhM2NiNjlmOTA1NTlkYyIsIklQIjoiMTg4LjE2Mi4xNDIuMTk4IiwiVUEiOiJNb3ppbGxhXC81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXRcLzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZVwvMTAzLjAuMC4wIFNhZmFyaVwvNTM3LjM2IiwiUmVmIjoiaHR0cHM6XC9cLzEwMS5ydVwvIiwidWlkX2NoYW5uZWwiOiIxNzgxNTEyIiwidHlwZV9jaGFubmVsIjoicGVyc29uYWwiLCJ0eXBlRGV2aWNlIjoiUEMiLCJCcm93c2VyIjoiQ2hyb21lIiwiQnJvd3NlclZlcnNpb24iOiIxMDMuMC4wLjAiLCJTeXN0ZW0iOiJXaW5kb3dzIDEwIiwiZXhwIjoxNjU3NTg0Mzg3fQ.BeKQXembGRdmwlOYGJyWyZJnKDgjqU7LHt6gVHtcaMo',
-        artist: 'denis.101.ru',
+        artist: 'Denis Disco',
         cover: 'images/cover/ed80.jpg'
     },
+
+
+
+{
+        name: 'news radio',
+        path: 'https://icecast-vgtrk.cdnvideo.ru/vestifm_aac_32kbps',
+        artist: 'Vesti FM',
+        cover: 'images/cover/vesti-fm.jpg'
+    },
     {
-        name: 'современное Italo Disco',
+        name: 'Italo Disco',
         path: 'https://ruby.torontocast.com:1525/stream',
         artist: 'New Generation',
         cover: 'images/cover/new.png'
     },
     {
-        name: 'классическое Italo Disco',
+        name: 'Italo Disco',
         path: 'https://ruby.torontocast.com:1385/stream',
         artist: 'Disco Classic',
         cover: 'images/cover/classic.png'
     },
     {
-        name: 'Евро Italo Disco',
+        name: 'Italo Disco',
         path: 'https://ruby.torontocast.com:1565/stream',
         artist: 'Euro Disco',
         cover: 'images/cover/euro-disco.png'
     },
 
 {
-        name: 'синтетическое Italo Disco',
+        name: 'Italo Disco',
         path: 'https://ruby.torontocast.com:1615/stream',
         artist: 'Synthesizer Dance',
         cover: 'images/cover/synt.png'
     },
 
 {
-        name: ' стиль 80х Italo Disco',
+        name: 'Italo Disco',
         path: 'https://ruby.torontocast.com:1635/stream',
         artist: '80s Gold',
         cover: 'images/cover/80s.png'
     },
-{
-        name: 'дискография',
-        path: 'https://str3.pcradio.ru/Jurij_Shatunov-med',
-        artist: 'Шатунов Ю.',
-        cover: 'images/cover/shatunov.jpg'
-    },
+
+
+
 
 
 
