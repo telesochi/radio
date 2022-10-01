cb11cb546bc11e0cdf50d4fa2c12c1b389349c68
Denis Deaf <98868526+telesochi@users.noreply.github.com>
2022-10-01 10:46:20 +0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,70 @@
+let songs = [
+    {
+        name: '',
+        path: 'https://ssl-1.radiohost.pl:8018/stream',
+        artist: 'Italo4You',
+        cover: 'images/cover/1.jpg'
+    },
+    
+
+
+{
+        name: '',
+        path: 'https://pub0102.101.ru:8443/stream/personal/aacp/64/1781512?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJrZXkiOiIwNjBhYzBkMDMzZDExZjg0NTFhM2NiNjlmOTA1NTlkYyIsIklQIjoiMTg4LjE2Mi4xNDIuMTk4IiwiVUEiOiJNb3ppbGxhXC81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXRcLzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZVwvMTAzLjAuMC4wIFNhZmFyaVwvNTM3LjM2IiwiUmVmIjoiaHR0cHM6XC9cLzEwMS5ydVwvIiwidWlkX2NoYW5uZWwiOiIxNzgxNTEyIiwidHlwZV9jaGFubmVsIjoicGVyc29uYWwiLCJ0eXBlRGV2aWNlIjoiUEMiLCJCcm93c2VyIjoiQ2hyb21lIiwiQnJvd3NlclZlcnNpb24iOiIxMDMuMC4wLjAiLCJTeXN0ZW0iOiJXaW5kb3dzIDEwIiwiZXhwIjoxNjU3NTg0Mzg3fQ.BeKQXembGRdmwlOYGJyWyZJnKDgjqU7LHt6gVHtcaMo',
+        artist: 'Denis Disco',
+        cover: 'images/cover/ed80.jpg'
+    },
+
+
+
+{
+        name: '',
+        path: 'https://icecast-vgtrk.cdnvideo.ru/vestifm_aac_32kbps',
+        artist: 'Vesti FM',
+        cover: 'images/cover/vesti-fm.jpg'
+    },
+    {
+        name: '',
+        path: 'https://ruby.torontocast.com:1525/stream',
+        artist: 'New Generation',
+        cover: 'images/cover/new.png'
+    },
+    {
+        name: '',
+        path: 'https://ruby.torontocast.com:1385/stream',
+        artist: 'Disco Classic',
+        cover: 'images/cover/classic.png'
+    },
+    {
+        name: '',
+        path: 'https://ruby.torontocast.com:1565/stream',
+        artist: 'Euro Disco',
+        cover: 'images/cover/euro-disco.png'
+    },
+
+{
+        name: '',
+        path: 'https://ruby.torontocast.com:1615/stream',
+        artist: 'Synthesizer Dance',
+        cover: 'images/cover/synt.png'
+    },
+
+{
+        name: '',
+        path: 'https://ruby.torontocast.com:1635/stream',
+        artist: '80s Gold',
+        cover: 'images/cover/80s.png'
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+]
